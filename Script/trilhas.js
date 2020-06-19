bac41dc330745ae3483b01b038fe4aac1a449cc2
mathuscada <46804012+mathuscada@users.noreply.github.com>
2020-06-19 17:35:32 -0300

    Update trilhas.js

--- Script/trilhas.js
+++ Script/trilhas.js
@@ -1,7 +1,8 @@
 class Path{
-    constructor(training, pathType){
+    constructor(training, pathType, talents){
         this._training = training;
         this._pathType = pathType;
+        this._talents = talents;
     }
     get training(){
         return this._training;
@@ -12,10 +13,44 @@ class Path{
     get pathType(){
         return this._pathType;
     }
+    set pathType(pathType){
+        this._pathType = pathType;
+    }
+    get talents(){
+        return this._talents;
+    }
+    addTalent(name, talent){
+        this._talents[name] = talent;
+    }
+    modifyTalentName(previousName, newName){
+        if(previousName in Object.keys(this._talents)){
+            this._talents[newName] = this._talents[previousName];
+            delete this._talents[previousName];
+            return true;
+        } else{
+            return false;
+        }
+    }
+    modifyTalent(name, talent){
+        if(name in Object.keys(this._talents)){
+            this._talents[name] = talent;
+            return true;
+        } else{
+            return false;
+        }
+    }
+    deleteTalent(name){
+        if(name in Object.keys(this._talents)){
+            delete this._talents[name];
+            return true;
+        } else{
+            return false;
+        }
+    }
 }
 
-class Magician extends Path{
-    constructor(training, attributes, languages, academicArea){
+class Warrior extends Path{
+    constructor(training){
         
     }
 }
